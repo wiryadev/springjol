8c020c6ad3be1c3d8e3a2f6a8f2712ca1f2128cd
Abrar Wiryawan <abrarwiryawan@gmail.com>
2022-12-23 11:20:48 +0700

    exclude some class

--- src/main/kotlin/com/wiryadev/springjol/SpringjolApplication.kt
+++ src/main/kotlin/com/wiryadev/springjol/SpringjolApplication.kt
@@ -1,9 +1,14 @@
 package com.wiryadev.springjol
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+	exclude = [
+		UserDetailsServiceAutoConfiguration::class,
+	]
+)
 class SpringjolApplication
 
 fun main(args: Array<String>) {
